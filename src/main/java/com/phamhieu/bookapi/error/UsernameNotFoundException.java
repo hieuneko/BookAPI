46acd89e40c163112233062da46d8c82ad0e733f
hieuneko <hieuneko@gmail.com>
2023-03-24 00:52:20 +0700

    update class and unit test

--- src/main/java/com/phamhieu/bookapi/error/UsernameNotFoundException.java
+++ src/main/java/com/phamhieu/bookapi/error/UsernameNotFoundException.java
@@ -1,10 +1,8 @@
 package com.phamhieu.bookapi.error;
 
-import static java.lang.String.format;
-
 public class UsernameNotFoundException extends NotFoundException{
 
-    public UsernameNotFoundException(final String username, Object... args) {
+    public UsernameNotFoundException(final String username) {
         super("User has username: %s not be found", username);
     }
 }
