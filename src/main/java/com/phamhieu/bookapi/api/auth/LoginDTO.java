14d8dfea01778b909249e732b2d3ff319e9c7bbb
hieuneko <hieuneko@gmail.com>
2023-03-28 16:36:21 +0700

    fix login security logic

--- src/main/java/com/phamhieu/bookapi/api/auth/LoginDTO.java
+++ src/main/java/com/phamhieu/bookapi/api/auth/LoginDTO.java
@@ -4,7 +4,6 @@ package com.phamhieu.bookapi.api.auth;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
 @Builder
--- src/main/java/com/phamhieu/bookapi/domain/book/Book.java
+++ src/main/java/com/phamhieu/bookapi/domain/book/Book.java
@@ -3,7 +3,6 @@ package com.phamhieu.bookapi.domain.book;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
-import lombok.With;
 
 import java.time.Instant;
 import java.util.UUID;
