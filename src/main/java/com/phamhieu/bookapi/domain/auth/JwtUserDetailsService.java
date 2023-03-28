73fe8bfe2b2ad90710e639520828d32c56568ebe
hieuneko <hieuneko@gmail.com>
2023-03-28 17:02:24 +0700

    fix login security logic

--- src/main/java/com/phamhieu/bookapi/domain/auth/JwtUserDetailsService.java
+++ src/main/java/com/phamhieu/bookapi/domain/auth/JwtUserDetailsService.java
@@ -1,13 +1,14 @@
 package com.phamhieu.bookapi.domain.auth;
 
 
-import com.phamhieu.bookapi.error.UsernameNotFoundException;
 import com.phamhieu.bookapi.persistence.role.RoleStore;
 import com.phamhieu.bookapi.persistence.user.UserStore;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -26,7 +27,7 @@ public class JwtUserDetailsService implements UserDetailsService {
                 .orElseThrow(() -> new UsernameNotFoundException(username));
     }
 
-    private UserDetails buildUser(final com.phamhieu.bookapi.domain.user.User user) {
+    private User buildUser(final com.phamhieu.bookapi.domain.user.User user) {
         final String role = roleStore.findRoleName(user.getRoleId());
         return new JwtUserDetails(user,
                 List.of(new SimpleGrantedAuthority(role)));
