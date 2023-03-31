1fe7df561673f489aac9d1d8212af377652333b9
hieuneko <hieuneko@gmail.com>
2023-03-31 16:29:39 +0700

    fix unit test

---
+++ src/test/java/com/phamhieu/bookapi/FirebaseInitializerTest.java
@@ -0,0 +1,26 @@
+package com.phamhieu.bookapi;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FirebaseInitializerTest {
+
+    @Test
+    void shouldInitializer_OK() throws IOException {
+        FirebaseApp.getApps();
+        final InputStream serviceAccount = FirebaseInitializer.class.getResourceAsStream("/BookApiAccountKeyTest.json");
+        final GoogleCredentials credentials = GoogleCredentials.fromStream(serviceAccount);
+        final FirebaseOptions options = new FirebaseOptions.Builder()
+                .setCredentials(credentials)
+                .build();
+        FirebaseApp.initializeApp(options);
+        assertNotNull(FirebaseApp.getApps());
+    }
+}
