2237b125f6299d346da12b32b7e57eda529c6d98
hieuneko <hieuneko@gmail.com>
2023-03-31 15:59:40 +0700

    fix unit test

--- src/test/java/com/phamhieu/bookapi/BookApiApplicationTests.java
+++ src/test/java/com/phamhieu/bookapi/BookApiApplicationTests.java
@@ -2,8 +2,12 @@ package com.phamhieu.bookapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@ContextConfiguration(classes = BookApiApplicationTests.class)
 class BookApiApplicationTests {
 
     @Test
